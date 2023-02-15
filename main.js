e7f23db88cfc5ccb2377c89f0fa7cd6b7b0041dd
alex-lee13 <122317975+alex-lee13@users.noreply.github.com>
2023-02-14 19:19:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-let isclicked = false;
-
+// This function adds a border when a circle is clicked
+// As well as gets the attributes of the svgs
 function addBorder() {
-  if (!isclicked) {
-    document.getElementById('circ').classList.add("border");
-    isclicked = true;
-  }
-  
 
-  else {
-    document.getElementById('circ').classList.remove("border");
-    isclicked = false;
-  }
-}
+   this.classList.toggle("border-item");
 
+   let x = this.getAttribute('cx');
+   let y = this.getAttribute('cy');
 
-  document.getElementById("circ").addEventListener("click", addBorder);
+   // Scaled the x and y coords
+   let scaled_x = x/30;
+   let scaled_y = 10 - y/30;
+
+   // Replaces the null paragraph in the right div with text of the last point clicked
+   let newtext = 'Last point clicked' + ' ' + '('+ scaled_x + ',' + ' ' + scaled_y + ')';
+   document.getElementById('coord_text').innerHTML=newtext;
+
+ }
+
+// gets a list of circles using classname
+ // Loops over the list and adds an event listener for each circle clicked
+let circs = document.getElementsByClassName('circ');
+for (let i = 0; i < circs.length; i++) {
+   circs[i].addEventListener("click", addBorder)};
